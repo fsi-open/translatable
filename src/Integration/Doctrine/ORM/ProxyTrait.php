34e7c298fef5848077e10c657fc54497ea0b9c01
Lukasz Cybula <lukasz@fsi.pl>
2024-06-19 16:54:08 +0200

    Allowed Symfony 6.x components and doctrine/orm ^3.0

---
+++ src/Integration/Doctrine/ORM/ProxyTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FSi\Component\Translatable\Integration\Doctrine\ORM;
+
+use Doctrine\Persistence\Proxy;
+
+trait ProxyTrait
+{
+    private function initializeProxy(): void
+    {
+        if ($this instanceof Proxy) {
+            $this->__load();
+        }
+    }
+}
